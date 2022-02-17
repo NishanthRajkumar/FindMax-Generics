d81148f425005dcf9f1e720f481d26015c68db51
Nishanth <rajkumarnishanth@gmail.com>
2022-02-17 13:49:17 +0530

    Generic print method to print max value

--- MaximumGenerics/Maximum.cs
+++ MaximumGenerics/Maximum.cs
@@ -2,15 +2,32 @@
 
 internal class Maximum<T> where T : IComparable
 {
+    // T array for sorting and to find max
     private T[] array;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Maximum{T}"/> class.
+    /// </summary>
     public Maximum(T[] array)
     {
         this.array = array;
     }
+
+    /// <summary>
+    /// Determines the maximum of the parameters.
+    /// </summary>
     public void Max(params T[] array)
     {
         this.array = array;
         Array.Sort(array);
+        PrintMax(array);
+    }
+
+    /// <summary>
+    /// Prints the maximum.
+    /// </summary>
+    private void PrintMax(T[] value)
+    {
         Console.WriteLine($"Max value is: {array[2]}");
     }
 }
