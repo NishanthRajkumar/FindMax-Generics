c7602e9110d4d1d882a81f631b61a3ae7730f68d
Nishanth <rajkumarnishanth@gmail.com>
2022-02-17 13:09:32 +0530

    Refactored: Generic Max method; type extends Icomparable

--- MaximumGenerics/Maximum.cs
+++ MaximumGenerics/Maximum.cs
@@ -1,43 +1,11 @@
 ﻿namespace MaximumGenerics;
 
-internal static class Maximum
+internal static class Maximum<T> where T : IComparable
 {
-    /// <summary>
-    /// Finds Max of 3 integers
-    /// </summary>
-    public static void IntMax(int num1, int num2, int num3)
+    public static void Max(T var1, T var2, T var3)
     {
-        if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
-            Console.WriteLine($"Max value is: {num1}");
-        else if (num2.CompareTo(num3) >= 0)
-            Console.WriteLine($"Max value is: {num2}");
-        else
-            Console.WriteLine($"Max value is: {num3}");
-    }
-
-    /// <summary>
-    /// Finds Max of 3 floats
-    /// </summary>
-    public static void FloatMax(float num1, float num2, float num3)
-    {
-        if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
-            Console.WriteLine($"Max value is: {num1}");
-        else if (num2.CompareTo(num3) >= 0)
-            Console.WriteLine($"Max value is: {num2}");
-        else
-            Console.WriteLine($"Max value is: {num3}");
-    }
-
-    /// <summary>
-    /// Finds Max of 3 strings
-    /// </summary>
-    public static void StringMax(string str1, string str2, string str3)
-    {
-        if (str1.CompareTo(str2) >= 0 && str1.CompareTo(str3) >= 0)
-            Console.WriteLine($"Max value is: {str1}");
-        else if (str2.CompareTo(str3) >= 0)
-            Console.WriteLine($"Max value is: {str2}");
-        else
-            Console.WriteLine($"Max value is: {str3}");
+        T[] array = new T[] { var1, var2, var3 };
+        Array.Sort(array);
+        Console.WriteLine($"Max value is: {array[2]}");
     }
 }
